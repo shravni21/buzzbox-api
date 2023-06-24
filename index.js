9f17eb779134f77d976de00e9a012cb513e8318b
Shravni Wakde <wakdeshravni@gmail.com>
2023-06-24 15:42:00 +0530

    chal jaa yarrr

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.use("/posts", postRoutes);
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 3001;
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
