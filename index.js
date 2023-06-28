7d959ac49a0a2f46bf23bcb192f8c207a6b150f6
Shravni Wakde <wakdeshravni@gmail.com>
2023-06-28 16:07:37 +0530

    data added successfully

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
 import Post from "./models/Post.js";
-import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -52,9 +51,6 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
-/* ADD DATA ONE TIME */
-// User.insertMany(users);
-// Post.insertMany(posts);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 3001;
